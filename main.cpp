d10e9fdbdb46bc2c4572df88815d16b4a2f7452f
FerretLeader <42744568+FerretLeader@users.noreply.github.com>
2021-02-01 19:11:20 -0600

    First commit
    Only some basic work has been done

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*******************************************************
+/ Mame: main.cpp
+/ Creator: Michael Lee Brummitt
+/ Purpose: Main file for the TicTacToe program. This is
+/ the file that you should compile
+/ Version: 0.1
+*******************************************************/
+
+using namespace std;
+
+#include "myGrid.h"
+
+int main(){
+	bool continue = true;
+	
+	// loop until it is time to exit
+	while (continue == true){
+		
+	}
+}
---
+++ myGrid.h
@@ -0,0 +1,15 @@
+/*******************************************************
+/ Mame: myGrid.h
+/ Creator: Michael Lee Brummitt
+/ Purpose: This file contains a myGrid class which will
+/ allow you to create a grid with a size that you can
+/ define.
+/ Version: 0.1
+*******************************************************/
+
+class myGrid{
+	private:
+		
+	public:
+		
+};
